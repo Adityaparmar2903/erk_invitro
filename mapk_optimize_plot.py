7e5002d219c12fcfe0dc5522309f136137e9f54e
Aditya Parmar <aditya@lsploancbookair.private.wireless.med.harvard.edu>
2016-06-29 14:32:43 -0400

    Plot code organised with functions

--- mapk_optimize_plot.py
+++ mapk_optimize_plot.py
@@ -5,271 +5,189 @@ import numpy as np
 import pickle
 from scipy.stats.distributions import norm
 from matplotlib import pyplot as plt
-import plotly.plotly as py
-import plotly.graph_objs as go
+#import plotly.plotly as py
+#import plotly.graph_objs as go
 
-#list the algorithms
 method_list = ['Nelder-Mead', 'Powell', 'COBYLA', 'TNC']
-
-#list the models
 model_list = [build_markevich_2step, build_erk_autophos_any,
-	      build_erk_autophos_uT, build_erk_autophos_phos,
-	      build_erk_activate_mkp]
+          build_erk_autophos_uT, build_erk_autophos_phos,
+          build_erk_activate_mkp]
+model_list_name = ['build_markevich_2step', 'build_erk_autophos_any',
+          'build_erk_autophos_uT', 'build_erk_autophos_phos',
+          'build_erk_activate_mkp']
 
-#no. of initial values sampled using Latin Hypercube Sampling
-num_ini = 1000
-from_id = 0
-to_id = 1000
+num_ini = 200
 num_top = 10
-
-#read the data
-data = read_data()
-
 obj_func = np.ones((len(method_list),len(model_list),num_ini))
 obj_func[:] = np.nan
 func_eval = np.ones((len(method_list),len(model_list),num_ini))
 func_eval[:] = np.nan
-ave_obj_func = np.ones((len(method_list),len(model_list)))
-ave_func_eval = np.ones((len(method_list),len(model_list)))
 obj_func_top = np.ones((len(method_list),len(model_list),num_top))
 obj_func_top[:] = np.nan
-func_eval_top = np.ones((len(method_list),len(model_list),num_top))
-func_eval_top[:] = np.nan
-obj_func_sort = np.ones((len(method_list),len(model_list),num_ini))
-obj_func_sort[:] = np.nan
-ind_best = np.ones((len(method_list),len(model_list)))
-ind_best[:] = np.nan
+ave_obj_func = np.ones((len(method_list),len(model_list)))
+ave_func_eval = np.ones((len(method_list),len(model_list)))
 arr_ind = np.empty((len(method_list),len(model_list),num_ini))
-arr_ind_sort = np.empty((len(method_list),len(model_list),num_ini))
-
 run_ind = np.arange(num_ini)
 top_ind = np.arange(num_top)
-
 colors = ["red","blue","green","yellow","cyan","darkviolet","sienna","darkgray","black"]
 mark = ["o","^","D","s","H","x","+","."]
 plt.ion()
 
-#read the pkl files and store the result
-for i in range(len(method_list)):
-	for j in range(len(model_list)):
-		for k in range(0,1000):
-			arr_ind[i][j][k] = k
-			fname = "output_final/%s_%s_%d.pkl" % (i,j,k)
-			if not os.path.exists(fname):
-				print fname + ' does not exist.'
-				continue
-			results = pickle.load(open(fname, "rb"))
-			l = k%5
-			result = results[l]
-			if np.isnan(result.fun) or np.isinf(result.fun):
+def read_pkl(list1, list2, num, data1, data2, data3, data4, data5):
+    for i in range(len(list1)):
+    	for j in range(len(list2)):
+    		for k in range(num):
+    			data5[i][j][k] = k
+    			fname = "output_powell_subprob/%s_%s_%d.pkl" % (i,j,k)
+    			if not os.path.exists(fname):
+    				print fname + ' does not exist.'
     				continue
-			obj_func[i][j][k] = result.fun
-			func_eval[i][j][k] = result.nfev
-		ave_obj_func[i][j] = np.nanmean(obj_func[i][j])
-		ave_func_eval[i][j] = np.nanmean(func_eval[i][j])
+    			result = pickle.load(open(fname, "rb"))
+    			if np.isnan(result.fun) or np.isinf(result.fun):
+        				continue
+    			data1[i][j][k] = result.fun
+    			data2[i][j][k] = result.nfev
+    		data3[i][j] = np.nanmean(obj_func[i][j])
+    		data4[i][j] = np.nanmean(func_eval[i][j])
 
-"""PLOT_FIT"""
-for ii in range(len(method_list)):
-	for jj in range(len(model_list)):
-		for kk in range(num_top):
-			obj_func_top[ii][jj][kk] = (np.sort(obj_func[ii][jj]))[kk]
+def sort_run_ind(list1, list2, data):
+    arr_ind_sort = np.empty((len(list1), len(list2), num_ini))
+    ind_best = np.ones((len(list1),len(list2)))
+    ind_best[:] = np.nan
+    for i in range(len(list1)):
+        for j in range(len(list2)):
+            inds = data[i][j].argsort()
+            arr_ind_sort[i][j] = arr_ind[i][j][inds]
+    for i in range(len(list1)):
+        for j in range(len(list2)):
+            ind_best[i][j] = arr_ind_sort[i][j][0]
+    ind_best = np.int_(ind_best)
+    return ind_best
 
-for ii in range(len(method_list)):
-	for jj in range(len(model_list)):
-		inds = obj_func[ii][jj].argsort()
-		arr_ind_sort[ii][jj] = arr_ind[ii][jj][inds]
-		
+def plot_fit_imp(list1, list2):
+    for r in range(len(list1)):
+        for s in range(len(list2)):
+            best_ind = sort_run_ind(list1, list2, obj_func)
+            t = best_ind[r][s]
+            fname1 = "output_powell_subprob/%s_%s_%d.pkl" % (r,s,t)
+            if not os.path.exists(fname1):
+                print fname1 + ' does not exist.'
+                continue
+            result1 = pickle.load(open(fname1, "rb"))
+            if np.isnan(result1.fun) or np.isinf(result1.fun):
+                    continue
+            best_p = result1.x
+            model = model_list[s]()
+            pd = parameter_dict(model, best_p)
+            plot_fit(model, data, pd)
+            plt.suptitle("%s-%s" % (method_list[r], model_list[s].__name__),
+                        fontsize=14, fontweight='bold')
+            plt.savefig("Plots/Plot_fit/%s-%s.png"
+                        % (method_list[r], model_list[s].__name__),
+                        format='png')
 
-for ii in range(len(method_list)):
-	for jj in range(len(model_list)):
-		ind_best[ii][jj] = arr_ind_sort[ii][jj][0]
+def plot_obj_func(t, func, list1, list2, **keyword):
+    for a in range(len(list1)):
+        plt.figure()
+        plt.suptitle(list1[a], fontsize=14, fontweight='bold')
+        for b in range(len(list2)):
+            if ('Method' in keyword):
+                plt.plot(t, np.sort(func[a][b]), linestyle='solid', marker='.',
+                            markeredgewidth=0.0, color=colors[b],
+                            label=list2[b])
+            else:
+                plt.plot(t, np.sort(func[b][a]), linestyle='solid', marker='.',
+                            markeredgewidth=0.0, color=colors[b],
+                            label=list2[b])
 
-ind_best = np.int_(ind_best)
+        plt.legend(loc='upper left')
+        plt.ylim((0,8000))
+        plt.xlabel('Run index')
+        plt.ylabel('Objective function value')
+        plt.grid(True)
+        if ('Top10' in keyword):
+            plt.savefig("Plots/Top10-%s.png" % (list1[a]),
+            format='png')
+        else:
+            plt.savefig("Plots/%s.png" % (list1[a]),
+            format='png')
 
-for rr in range(len(method_list)):
-	for ss in range(len(model_list)):
-		tt = ind_best[rr][ss]
-		fname1 = "output_final/%s_%s_%d.pkl" % (rr,ss,tt)
-		if not os.path.exists(fname1):
-			print fname1 + ' does not exist.'
-			continue
-		results1 = pickle.load(open(fname1, "rb"))
-		ll = tt%5
-		result1 = results1[ll]
-		if np.isnan(result1.fun) or np.isinf(result1.fun):
-    			continue
-		best_p = result1.x
-		model = model_list[ss]()
-		pd = parameter_dict(model, best_p)
-		
-		plot_fit(model,data,pd)
-		manager = plt.get_current_fig_manager()
-		manager.resize(*manager.window.maxsize())
-		
-		plt.suptitle("%s-%s" % (method_list[rr], model_list[ss].__name__), fontsize=14, 					fontweight='bold')
-		plt.savefig("Plots/Plot_fit/%s-%s.png" % (method_list[rr], model_list[ss].__name__),
-				format='png')
-		plt.savefig("Plots/Plot_fit/%s-%s.pdf" % (method_list[rr], model_list[ss].__name__),
-				format='pdf')
-		
-		
-"""OBJECTIVE FUNCTION"""
-#for each algorithm plot the results for different models
-for a in range(len(method_list)):
-	plt.figure()
-	plt.suptitle(method_list[a], fontsize=14, fontweight='bold')
-	for b in range(len(model_list)):
-		plt.plot(run_ind+1, np.sort(obj_func[a][b]), linestyle='',
-			marker='.', markeredgewidth=0.0, color = colors[b],
-			label = model_list[b].__name__)
-	plt.legend(loc='upper left')
-	plt.xlabel("Run index")
-	plt.ylabel("Objective function value")
-	#plt.ylim(-1e5,0)
-	plt.grid(True)
-	#plt.savefig("Plots/Method/%s.pdf" % (method_list[a]),format='pdf')
-	plt.savefig("Plots/Method/%s.png" % (method_list[a]),format='png')
-		
-#for each model plot the results for different algorithms
-for aa in range(len(model_list)):
-	plt.figure()
-	plt.suptitle(model_list[aa].__name__, fontsize=14, fontweight='bold')
-	for bb in range(len(method_list)):
-		plt.plot(run_ind+1, np.sort(obj_func[bb][aa]), linestyle='', 
-			marker='.', markeredgewidth=0.0, color = colors[bb],
-			label = method_list[bb])
-	plt.legend(loc='upper left')
-	plt.xlabel("Run index")
-	plt.ylabel("Objective function value")
-	#plt.ylim(-1e5,0)
-	plt.grid(True)
-	#plt.savefig("Plots/Model/%s.pdf" % (model_list[aa].__name__),format='pdf')
-	plt.savefig("Plots/Model/%s.png" % (model_list[aa].__name__),format='png')
+def plot_performance(name, xaxis, yaxis, **keyword):
+    plt.figure()
+    plt.suptitle(name, fontsize=14, fontweight='bold')
+    if ('model' in keyword):
+        for a in range(len(model_list)):
+            plt.plot(xaxis[:,a], yaxis[:,a], linestyle='solid',
+                    linewidth=3.0, color=colors[a],
+                    label=model_list_name[a])
+    else:
+        for a in range(len(method_list)):
+            plt.plot(xaxis[a], yaxis[a], linestyle='solid',
+                    linewidth=3.0, color=colors[a],
+                    label=method_list[a])
+    plt.legend(loc='upper left')
+    plt.xlabel("Objective function value (Average)")
+    plt.ylabel("Calls to the objective function (Average)")
+    plt.grid(True)
+    if('model' in keyword):
+            plt.savefig("Plots/Performance/Model_Average_Performance.png",
+                        format='png')
+    else:
+            plt.savefig("Plots/Performance/Method_Average_Performance.png",
+                        format='png')
 
-#for each algorithm plot best results for different models  top 10
-for w in range(len(method_list)):
-	plt.figure()
-	plt.suptitle(method_list[w], fontsize=14, fontweight='bold')
-	for v in range(len(model_list)):
-		plt.plot(top_ind+1, obj_func_top[w][v], linestyle='solid',
-			marker='o', markeredgewidth=0.0, color = colors[v],
-			label = model_list[v].__name__)
-	plt.legend(loc='upper left')
-	plt.xlabel("Run index")
-	plt.ylabel("Objective function value")
-	#plt.ylim(-1e5,0)
-	plt.grid(True)
-	#plt.savefig("Plots/Method/Top10-%s.pdf" % (method_list[w]),format='pdf')
-	plt.savefig("Plots/Method/Top10-%s.png" % (method_list[w]),format='png')
+def plot_heatmap(data, **keyword):
+    row_labels = method_list
+    column_labels = [model_list_name[g] for g in range(len(model_list))]
+    if ('calls' in keyword):
+        data = np.transpose(ave_func_eval)
+        fig, ax = plt.subplots()
+        plt.suptitle('Objective Function Evaluations (Average)', fontsize=14,
+                      fontweight='bold')
+        heatmap = ax.pcolor(data, cmap=plt.cm.Reds)
+    else:
+        data = np.transpose(ave_obj_func)
+        fig, ax = plt.subplots()
+        plt.suptitle('Objective Function Value (Average)', fontsize=14,
+                      fontweight='bold')
+        heatmap = ax.pcolor(data, cmap=plt.cm.Blues)
+    # put the major ticks at the middle of each cell
+    ax.set_xticks(np.arange(data.shape[1])+0.5, minor=False)
+    ax.set_yticks(np.arange(data.shape[0])+0.5, minor=False)
+    # want a more natural, table-like display
+    ax.invert_yaxis()
+    ax.xaxis.tick_top()
+    ax.set_xticklabels(row_labels, minor=False)
+    ax.set_yticklabels(column_labels, minor=False)
+    plt.show()
+    if ('calls' in keyword):
+        plt.savefig("Plots/Heatmap/heatmap_func_eval2.png",format='png')
+    else:
+        plt.savefig("Plots/Heatmap/heatmap_obj_func2.png",format='png')
 
+data = read_data()
+read_pkl(method_list, model_list, num_ini, obj_func, func_eval, ave_obj_func, ave_func_eval, arr_ind)
 
-#for each model plot best results for different algorithms top 10
-for y in range(len(model_list)):
-	plt.figure()
-	plt.suptitle(model_list[y].__name__, fontsize=14, fontweight='bold')
-	for z in range(len(method_list)):
-		plt.plot(top_ind+1, obj_func_top[z][y], linestyle='solid', 
-			marker='o', markeredgewidth=0.0, color = colors[z],
-			label = method_list[z])
-	plt.legend(loc='upper left')
-	plt.xlabel("Run index")
-	plt.ylabel("Objective function value")
-	#plt.ylim(-1e5,0)
-	plt.grid(True)
-	#plt.savefig("Plots/Model/Top10-%s.pdf" % (model_list[y].__name__),format='pdf')
-	plt.savefig("Plots/Model/Top10-%s.png" % (model_list[y].__name__),format='png')
+for i in range(len(method_list)):
+    for j in range(len(model_list)):
+        for k in range(num_top):
+            obj_func_top[i][j][k] = (np.sort(obj_func[i][j]))[k]
 
+plot_fit_imp(method_list, model_list)
+plot_obj_func(run_ind+1, obj_func, method_list, model_list_name, Method=True)
+plot_obj_func(top_ind+1, obj_func_top, method_list, model_list_name, Method=True, Top10=True)
+plot_obj_func(run_ind+1, obj_func, model_list_name, method_list)
+plot_obj_func(top_ind+1, obj_func_top, model_list_name, method_list, Top10=True)
+plot_performance('Model_Performance', ave_obj_func, ave_func_eval ,model=True)
+plot_performance('Method_Performance', ave_obj_func, ave_func_eval)
+plot_heatmap(ave_obj_func)
+plot_heatmap(ave_func_eval, calls=True)
 
-
-"""HEATMAP"""
-#plot heatmap for objective function values
-column_labels = method_list
-row_labels = [model_list[g].__name__ for g in range(len(model_list))]
-data = ave_obj_func
-fig, ax = plt.subplots()
-plt.suptitle('Objective Function Value (Average)', fontsize=14, fontweight='bold')
-heatmap = ax.pcolor(data, cmap=plt.cm.Blues)
-
-# put the major ticks at the middle of each cell
-ax.set_xticks(np.arange(data.shape[1])+0.5, minor=False)
-ax.set_yticks(np.arange(data.shape[0])+0.5, minor=False)
-
-# want a more natural, table-like display
-ax.invert_yaxis()
-ax.xaxis.tick_top()
-
-ax.set_xticklabels(row_labels, minor=False)
-ax.set_yticklabels(column_labels, minor=False)
-plt.show() 
-plt.savefig("Plots/Heatmap/heatmap_obj_func2.png",format='png')
-#plt.savefig("Plots/Heatmap/heatmap_obj_func2.pdf",format='pdf')
-
-
-#plot heatmap for no. of function evaluations
-column_labels = method_list
-row_labels = [model_list[h].__name__ for h in range(len(model_list))]
-data = ave_func_eval
-fig, ax = plt.subplots()
-plt.suptitle('Objective Function Evaluations (Average)', fontsize=14, fontweight='bold')
-heatmap = ax.pcolor(data, cmap=plt.cm.Reds)
-
-# put the major ticks at the middle of each cell
-ax.set_xticks(np.arange(data.shape[1])+0.5, minor=False)
-ax.set_yticks(np.arange(data.shape[0])+0.5, minor=False)
-
-# want a more natural, table-like display
-ax.invert_yaxis()
-ax.xaxis.tick_top()
-
-ax.set_xticklabels(row_labels, minor=False)
-ax.set_yticklabels(column_labels, minor=False)
-plt.show()
-plt.savefig("Plots/Heatmap/heatmap_func_eval2.png",format='png')
-#plt.savefig("Plots/Heatmap/heatmap_func_eval2.pdf",format='pdf')
-
-
-
-"""MODEL PERFORMANCE"""
-plt.figure()
-plt.suptitle('Model performance', fontsize=14, fontweight='bold')
-#objective function vs function evaluations
-for aaa in range(len(model_list)):
-	#for bbb in range(len(method_list)):
-	plt.plot(ave_obj_func[:,aaa], ave_func_eval[:,aaa], linestyle='solid', 
-			linewidth= 3.0, color=colors[aaa],
-			label=model_list[aaa].__name__)
-	plt.legend(loc='upper right')
-	plt.xlabel("Objective function value (Average)")
-	plt.ylabel("Calls to the objective function (Average)")
-	plt.grid(True)
-plt.savefig("Plots/Performance/Model_Average_Performance.png",format='png')
-#plt.savefig("Plots/Performance/Model_Average_Performance.pdf",format='pdf')
-
-plt.figure()
-plt.suptitle('Method performance', fontsize=14, fontweight='bold')
-#objective function vs function evaluations
-for aaa in range(len(method_list)):
-	#for bbb in range(len(method_list)):
-	plt.plot(ave_obj_func[aaa], ave_func_eval[aaa], linestyle='solid', 
-			linewidth= 3.0, color=colors[aaa],
-			label=method_list[aaa])
-	plt.legend(loc='upper right')
-	plt.xlabel("Objective function value (Average)")
-	plt.ylabel("Calls to the objective function (Average)")
-	plt.grid(True)
-plt.savefig("Plots/Performance/Method_Average_Performance.png",format='png')
-#plt.savefig("Plots/Performance/Method_Average_Performance.pdf",format='pdf')
-
-
-"""HEATMAP plotly"""
+"""
+#HEATMAP plotly
 data_heat = [go.Heatmap(z=ave_obj_func,x=[(model_list[h].__name__) for h in range(len(model_list))],y=method_list)]
 py.iplot(data_heat, filename='heatmap_obj_func')
 
 data_heat = [go.Heatmap(z=ave_func_eval,x=[(model_list[h].__name__) for h in range(len(model_list))],y=method_list)]
 py.iplot(data_heat, filename='heatmap_func_eval')
-		
-
-	
-				
-
-				
+"""
