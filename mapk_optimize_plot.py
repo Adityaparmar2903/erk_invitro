44ef6faa1e8ac19e01b1dbabc7d25a6c29ece8bd
Aditya Parmar <Aditya.parmar2903@gmail.com>
2016-07-27 14:17:11 -0400

    Code for plotting objective function value sync to sorting of a particular method added

--- mapk_optimize_plot.py
+++ mapk_optimize_plot.py
@@ -1,6 +1,6 @@
 import os
-from mapk_model import model
-from run_mapk_mcmc import *
+#from mapk_model import model
+#from run_mapk_mcmc import *
 import numpy as np
 import pickle
 from scipy.stats.distributions import norm
@@ -8,17 +8,16 @@ from matplotlib import pyplot as plt
 #import plotly.plotly as py
 #import plotly.graph_objs as go
 
-#method_list = ['Nelder-Mead', 'Powell', 'COBYLA', 'TNC']
-method_list = ['trust-ncg', 'Powell']
+method_list = ['Nelder-Mead', 'Powell', 'CG', 'BFGS', 'L-BFGS-B',
+               'TNC', 'COBYLA', 'SLSQP', 'Newton-CG', 'trust-ncg', 'dogleg']
 model_list = [build_markevich_2step, build_erk_autophos_any,
           build_erk_autophos_uT, build_erk_autophos_phos,
           build_erk_activate_mkp]
-model_list_name = ['build_markevich_2step', 'build_erk_autophos_any',
-          'build_erk_autophos_uT', 'build_erk_autophos_phos',
-          'build_erk_activate_mkp']
+#model_list = ['robertson']
+#model_list_name = ['robertson']
 
-num_ini = 10
-num_top = 2
+num_ini = 300
+num_top = 10
 obj_func = np.ones((len(method_list),len(model_list),num_ini))
 obj_func[:] = np.nan
 func_eval = np.ones((len(method_list),len(model_list),num_ini))
@@ -30,7 +29,8 @@ ave_func_eval = np.ones((len(method_list),len(model_list)))
 arr_ind = np.empty((len(method_list),len(model_list),num_ini))
 run_ind = np.arange(num_ini)
 top_ind = np.arange(num_top)
-colors = ["red","blue","green","yellow","cyan","darkviolet","sienna","darkgray","black"]
+colors = ['red', 'darkgreen', 'blue', 'cyan', 'magenta',
+         'yellow', 'orange', 'sienna', 'black', 'lawngreen', 'slategrey']
 mark = ["o","^","D","s","H","x","+","."]
 plt.ion()
 
@@ -39,7 +39,7 @@ def read_pkl(list1, list2, num, data1, data2, data3, data4, data5):
     	for j in range(len(list2)):
     		for k in range(num):
     			data5[i][j][k] = k
-    			fname = "output_trust_ncg/%s_%s_%d.pkl" % (i,j,k)
+    			fname = "output/%s_%s_%d.pkl" % (i,j,k)
     			if not os.path.exists(fname):
     				print fname + ' does not exist.'
     				continue
@@ -70,7 +70,7 @@ def plot_fit_imp(list1, list2):
         for s in range(len(list2)):
             best_ind = sort_run_ind(list1, list2, obj_func)
             t = best_ind[r][s]
-            fname1 = "output_trust_ncg/%s_%s_%d.pkl" % (r,s,t)
+            fname1 = "output_robertson/%s_%s_%d.pkl" % (r,s,t)
             if not os.path.exists(fname1):
                 print fname1 + ' does not exist.'
                 continue
@@ -105,6 +105,7 @@ def plot_obj_func(t, func, list1, list2, **keyword):
         plt.ylim((0,8000))
         plt.xlabel('Run index')
         plt.ylabel('Objective function value')
+        plt.ylim(0,3)
         plt.grid(True)
         if ('Top10' in keyword):
             plt.savefig("Plots/Top10-%s.png" % (list1[a]),
@@ -164,7 +165,21 @@ def plot_heatmap(data, **keyword):
     else:
         plt.savefig("Plots/Heatmap/heatmap_obj_func2.png",format='png')
 
-data = read_data()
+def plot_obj_func_sort(index):
+    plt.figure()
+    inds = obj_func[index].argsort()
+    for j in range(len(method_list)):
+        for t in range(model_list):
+            obj_func[j][t] = obj_func[j][t][inds]
+    for i, meth in enumerate(method_list):
+        plt.plot(ind+1, obj_func[i], linestyle = 'solid',
+                 marker='.', markeredgewidth=0.0, color = colors[i] , label = meth)
+        plt.legend()
+        plt.xlabel("Run Index")
+        plt.ylabel("Objective Function Value (log)")
+        plt.grid(True)
+
+#data = read_data()
 read_pkl(method_list, model_list, num_ini, obj_func, func_eval, ave_obj_func, ave_func_eval, arr_ind)
 
 for i in range(len(method_list)):
@@ -172,7 +187,7 @@ for i in range(len(method_list)):
         for k in range(num_top):
             obj_func_top[i][j][k] = (np.sort(obj_func[i][j]))[k]
 
-plot_fit_imp(method_list, model_list)
+#plot_fit_imp(method_list, model_list)
 plot_obj_func(run_ind+1, obj_func, method_list, model_list_name, Method=True)
 plot_obj_func(top_ind+1, obj_func_top, method_list, model_list_name, Method=True, Top10=True)
 plot_obj_func(run_ind+1, obj_func, model_list_name, method_list)
@@ -181,7 +196,7 @@ plot_performance('Model_Performance', ave_obj_func, ave_func_eval ,model=True)
 plot_performance('Method_Performance', ave_obj_func, ave_func_eval)
 plot_heatmap(ave_obj_func)
 plot_heatmap(ave_func_eval, calls=True)
-
+plot_obj_func_sort(1)
 """
 #HEATMAP plotly
 data_heat = [go.Heatmap(z=ave_obj_func,x=[(model_list[h].__name__) for h in range(len(model_list))],y=method_list)]
